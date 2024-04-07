1e1eb914f4ceb396f179f7414fade6963183d277
Silvino Miranda <silvino_miranda@hotmail.com>
2024-04-07 16:33:16 -0300

    chore: add error.component

---
+++ projects/ng-triple-exemple/src/app/_shared/components/error/error.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ErrorComponent } from './error.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ErrorComponent],
+  exports: [ErrorComponent]
+})
+export class ErrorModule {}
